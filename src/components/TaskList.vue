0e0b940acd1db371bb099d100f58bcdba6bd84e7
Bruno Coronado <brunocoronado@MacBook-Air-de-Bruno.local>
2022-09-29 15:16:41 -0500

    No muestra datos en consola

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,3 +1,25 @@
 <template>
   <h1>TaskList</h1>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { getTasks } from "../services/TaskServices";
+import { Task } from "../interfaces/Task";
+
+export default defineComponent({
+  data() {
+    return {
+      tasks: [] as Task[]
+    }
+  },
+  methods: {
+    async getAllTasks() {
+      const res = await getTasks();
+    },
+  },
+  mounted() {
+    this.getAllTasks();
+  },
+});
+</script>
--- src/services/TaskServices.ts
+++ src/services/TaskServices.ts
@@ -1,6 +1,10 @@
 import { axiosInstance } from "./axios";
 import { Task } from "../interfaces/Task";
 
+export const getTasks = async () => {
+  await axiosInstance.get("/tasks");
+};
+
 export const createTask = async (task: Task) => {
   await axiosInstance.post("/create", task);
 };
